4e0dd76742f5b7d214b3dfdc1b88becbebe4856a
afiez97 <mohdafiez7@gmail.com>
2024-09-03 06:05:04 +0800

    hepa aa szassasass lads sa A AS as aas dasd as as asas

--- api_picoms/api_hep/app/Http/Controllers/hep_aduanRespondenController.php
+++ api_picoms/api_hep/app/Http/Controllers/hep_aduanRespondenController.php
@@ -476,38 +476,45 @@ Mail::send('mail', $data, function ($message) use ($data, $EmailPurpose) {
         $year = $request->input('year');
 
         $obj = hep_aduanResponden::select(
-'*'
-            // 'mis_std_info.std_studentid',
-            // 'mis_std_info.sti_name',
-            // 'mis_std_info.sti_icno',
-            // 'hep_hostel.hostel_name',
-            // 'mis_prm_college.clg_name',
-            // 'hep_hostel_blok.block_name',
-            // 'hep_hostel_room.room_no',
-            // 'hep_aduanResponden.aduan_status',
-            // 'hep_aduanResponden.aduan_details',
-            // 'hep_aduanResponden.aduan_date',
-            // 'hep_jenisaduanHostel.aduan_nama'
+            'mis_std_info.std_studentid',
+            'mis_std_info.sti_name',
+            'mis_std_info.sti_icno',
+            'hep_hostel.hostel_name',
+            'mis_prm_college.clg_name',
+            'hep_hostel_blok.block_name',
+            'hep_hostel_room.room_no',
+            'hep_aduanResponden.aduan_status',
+            'hep_aduanResponden.aduan_details',
+            'hep_aduanResponden.aduan_date',
+            'hep_jenisaduanHostel.aduan_nama'
         )
             ->leftJoin('mis_std_info', 'hep_aduanResponden.FK_idStudent', '=', 'mis_std_info.std_studentid')
-            // ->leftJoin('mis_prm_college', 'hep_aduanResponden.FK_clg', '=', 'mis_prm_college.pk_id')
-            // ->leftJoin('hep_hostel', 'hep_aduanResponden.FK_hostel', '=', 'hep_hostel.hostel_id')
-            // ->leftJoin('hep_hostel_blok', 'hep_aduanResponden.FK_block', '=', 'hep_hostel_blok.block_id')
-            // ->leftJoin('hep_hostel_room', 'hep_aduanResponden.FK_room', '=', 'hep_hostel_room.room_id')
-            // ->leftJoin('hep_jenisaduanHostel', 'hep_jenisaduanHostel.pk_id', '=', 'hep_aduanResponden.FK_jenisaduanHostel')
+            ->leftJoin('mis_prm_college', 'hep_aduanResponden.FK_clg', '=', 'mis_prm_college.pk_id')
+            ->leftJoin('hep_hostel', 'hep_aduanResponden.FK_hostel', '=', 'hep_hostel.hostel_id')
+            ->leftJoin('hep_hostel_blok', 'hep_aduanResponden.FK_block', '=', 'hep_hostel_blok.block_id')
+            ->leftJoin('hep_hostel_room', 'hep_aduanResponden.FK_room', '=', 'hep_hostel_room.room_id')
+            ->leftJoin('hep_jenisaduanHostel', 'hep_jenisaduanHostel.pk_id', '=', 'hep_aduanResponden.FK_jenisaduanHostel')
             ->where('hep_aduanResponden.recordstatus', '!=', 'DEL');
-            // if($month){
-            //     // DD($month);
-            //     $obj = $obj->whereMonth('hep_aduanResponden.aduan_date', '=', $month);
-            // }
-            // if($year){
-            //     // DD('TEST2');
-            //     $obj = $obj->whereYear('hep_aduanResponden.aduan_date', '=', $year);
-            // }
+            if($month){
+                // DD($month);
+                $obj = $obj->whereMonth('hep_aduanResponden.aduan_date', '=', $month);
+            }
+            if($year){
+                // DD('TEST2');
+                $obj = $obj->whereYear('hep_aduanResponden.aduan_date', '=', $year);
+            }
             // else{
             //     $obj->whereYear('hep_aduanResponden.aduan_date', '=', date("Y"));
             // }
             
+// Dump the raw SQL and bindings
+dd($obj->toSql(), $obj->getBindings());
+
+// Execute the query to get data
+$data = $query->get();
+
+// Dump the data
+dd($data);
             $obj = $obj->get();
 
         // $records now contains the result of the query

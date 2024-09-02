e364bc9a94525d01c8f02641af8542ff65425fad
afiez97 <mohdafiez7@gmail.com>
2024-09-03 05:36:23 +0800

    hepa aa szassasass dsa dss sds sa sa as

--- api_picoms/api_hep/app/Http/Controllers/hep_aduanRespondenController.php
+++ api_picoms/api_hep/app/Http/Controllers/hep_aduanRespondenController.php
@@ -495,13 +495,12 @@ Mail::send('mail', $data, function ($message) use ($data, $EmailPurpose) {
             ->leftJoin('hep_hostel_room', 'hep_aduanResponden.FK_room', '=', 'hep_hostel_room.room_id')
             ->leftJoin('hep_jenisaduanHostel', 'hep_jenisaduanHostel.pk_id', '=', 'hep_aduanResponden.FK_jenisaduanHostel')
             ->where('hep_aduanResponden.recordstatus', '!=', 'DEL');
-            dd($month);
 
-            if($month){
+            if($month != ''){
                 // DD($month);
                 $obj = $obj->whereMonth('hep_aduanResponden.aduan_date', '=', $month);
             }
-            if($year){
+            if($year != ''){
                 // DD('TEST2');
                 $obj = $obj->whereYear('hep_aduanResponden.aduan_date', '=', $year);
             }

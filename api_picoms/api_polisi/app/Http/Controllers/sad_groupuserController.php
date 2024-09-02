7831554e761ffcebc14399c576394a2a7c4532dc
afiez97 <mohdafiez7@gmail.com>
2024-09-03 02:25:53 +0800

    sda s ss s s se ss s a s s s s ss s f s

--- api_picoms/api_polisi/app/Http/Controllers/sad_groupuserController.php
+++ api_picoms/api_polisi/app/Http/Controllers/sad_groupuserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use App\Models\ sad_groupuser;
+use App\Models\sad_groupuser;
 
 class sad_groupuserController extends Controller
 {
